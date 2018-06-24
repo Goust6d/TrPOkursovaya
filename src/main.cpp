53ca42fd8e226240ac74b3a090dcf4e8e10dbac3
root <root@Gd.localdomain>
2018-06-24 20:49:29 +0700

    Adding Generation Function

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,26 @@ using namespace std;
 
 int  i = 0;
 
+int Numgeneration(short int n, int A[])
+{   
+    srand(time(NULL));
+    A[0] = rand() %9 + 1;
+    	for (i = 1; i < n; i++)
+        {
+        	A[i] = rand() %10;      	
+        }
+		return(A[n]);
+}
+
 int main()
 {
+	short int n = 4;
+    int A[n], B[n], q;
+    Numgeneration(n, A);
     system("PAUSE");
     return 0;
 }
+
+
+
+
