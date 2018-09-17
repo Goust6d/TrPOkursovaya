700d28118d0bbd00a12302d01633aaf4ff4727b1
Twipz <darkgroup98@mail.ru>
2018-09-17 22:59:43 +0700

    Fixing menu errors and rules

--- src/main.c
+++ src/main.c
@@ -18,43 +18,44 @@ void cowandbull(int A[], int B[], short int n)
 			bull++;
 		}
 	}
-
 	for (i = 0; i < n; i++)
 	{
 		for ( j = 0; j < n; j++)
 		{
-			if (A[i] == B[j])
+			if ((A[i] == B[j]) && (A[i] != A[j]))
 			{
 				cow++;
 			}
 		}
 	}
-	cow = cow - bull;
 	printf("Num of bulls: %d     Num of cows: %d\n", bull, cow);
 }
 
 void game(int A[], int B[], short int n, int k)
 {
-	int q, kmax = 15;
-	printf("Enter the number:\n\n");
-	scanf("%i", &q);
+	int q, kmax = 30;
 	while ((q < 1000) || (q > 9999))
 	{
-		printf("Enter the number");
-		scanf("%i", &q);
+		printf("Enter the number: \n");
+		scanf("%d", &q);
 	}
 	bender(n, q, B);
 	cowandbull(A, B, n);
 	while (bull != n)
 	{
 		printf("Wrong number! Try again!\n\n");
-		scanf("%i", &q);
+		q = 0;
+		
+		while ((q < 1000) || (q > 9999))
+		{	
+			printf("Enter the number: \n");
+			scanf("%d", &q);
+		}
 		k++;
 		if (k == kmax)
 		{
 			printf("\nGame over!\n");
 			system("pause");
-			system("cls");
 			break;
 		}
 		bender(n, q, B);
@@ -65,7 +66,6 @@ void game(int A[], int B[], short int n, int k)
 		printf("You won! Congratulations!\n Back to main menu...");
 	}
 	system("pause");
-	system("cls");
 }
 
 int main()
@@ -73,72 +73,84 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	short int n = 4;
 	int k;
-	int A[4], B[4];
+	int A[n], B[n];
 	Numgeneration(n, A);
-	char c;
+	int c;
 
 	while (c!=4)
 	{
-		printf("1. Start game.\n2. Hardmode (15 tryes)\n3. Rules\nX for Exit.\n");
-		c = getchar();
+		printf("---[INFO]--- Use only 1-4 keys.\n\n1. Normal mode (30 tryes).\n2. Hardmode (15 tryes)\n3. Rules\n4. Exit.\n");
+		//c = getchar(); 
+		scanf("%d",&c);
 		switch (c)
 		{
-
-			case '1':
+			default:
 			{
-				system("cls");
-				k = 20;
-				game(A, B, n, k);
+				printf("You choose wrong number. Please use keys 1-4.\n");
+				system("pause");
 				break;
 			}
-			case '2':
+			case 1:
 			{
-				system("cls");
 				k = 1;
+				printf("---[INFO]---\nUse only keys 0-9\n\n");
 				game(A, B, n, k);
+				Numgeneration(n, A);
 				break;
 			}
-			case '3':
+			case 2:
 			{
-				system("cls");
-				FILE *f = fopen("Rules.txt", "r");
-				char ch;
-				printf(" ");
-				while (!feof(f))
+				k = 15;
+				printf("---[INFO]---\nUse only keys 0-9\n\n");
+				game(A, B, n, k);
+				Numgeneration(n, A);
+				break;
+			}
+			case 3:
+			{
+			/*	FILE *f;
+				char str[400];
+				char *estr;
+				f = fopen("/TrPOkursovaya/src/Rules.txt", "r");
+				if (f == NULL)
 				{
-					ch = getc(f);
-					if (ch == ' ')
+					printf("Error\n");
+					break;
+				}
+				while (1)
+				{
+					estr = fgets (str, sizeof(str), f);
+					if (estr == NULL)
 					{
-						k++;
+						if (feof(f)!=0)
+						{
+							break;
+						}
 					}
-					if (k == 10)
-					{
-						printf("\n");
-						k = 0;
-					}
-					printf("%c", ch);
+					printf ("    %s", str);
+				}
+				if (fclose(f) == EOF)
+				{
+					printf("Error\n");
 				}
 				printf("\n");
-				fclose(f);
-				system("pause");
-				system("cls");
+				break;*/
+				printf("The rules of the game Bulls and Cows\n\n");
+				printf("A certain number is guessed. You must guess it by typing with the keyboard.\n");
+				printf("When you enter a number you will be shown the number of Bulls and Cows in the number.\n");
+				printf("The number of Bulls is the number of digits that have coincided numerically and in the order of the place, in the number and number entered.\n");
+				printf("If the figure does not stand on this place, but it is present in the number conceived, then this figure is considered to be Cow.\n");
+				printf("In normal mode, you have 30 attempts.\n");
+				printf("In a complicated mode, the number of your attempts is limited to 15, so decide carefully. :-)\n\n");
 				break;
 			}
 
-			case 'x':
+			case 4:
 			{
 				return 0;
 			}
-			default:
-			{
-				system("cls");
-				printf("You choose wrong number. Please use keys 1-3 or ESC.\n");
-				system("pause");
-				system("cls");
-			}
 		}
 	}
-	system("PAUSE");
+	system("pause");
 	return 0;
-
 }
